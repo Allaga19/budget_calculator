eb92d2a88b533018847951c142f66acfb697509c
Allaga <Allaga59@yandex.ru>
2021-04-08 20:36:49 +0300

    создан проект "Калькулятор бюджета"

---
+++ js/script.js
@@ -0,0 +1,20 @@
+// alert('Hello');
+"use strict";
+const money = 30000,
+    income = 'Фриланс',  // дополнительные доходы
+    addExpenses = 'Садик, такси, интернет', // дополнительные расходы
+    deposin = true,
+    mission = 50000,
+    period = 6;
+let budgetDay = money / 30;
+
+    console.log(typeof money);
+    console.log(typeof income);
+    console.log(typeof deposit);
+    console.log(addExpenses.length);
+    console.log('Период равен ' + period + ' месяцев');
+    console.log('Цель заработать ' + mission + ' рублей/долларов/гривен/юани');
+    console.log(addExpenses.toLowerCase());
+    console.log(addExpenses.split(''));
+    console.log(budgetDay);
+    // console.log();
